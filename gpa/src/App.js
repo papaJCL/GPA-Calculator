55307cb3b76eca12f059be2b170166e5391402af
papaJCL <jeremylessermore@gmail.com>
2020-09-07 00:33:51 -0600

    Changed state to hooks

--- gpa/src/App.js
+++ gpa/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import { Input } from 'reactstrap'
-import { Row, Col, Container , Jumbotron } from 'react-bootstrap';
+import { Row, Col, Container, Jumbotron } from 'react-bootstrap';
 import Swal from 'sweetalert2'
-// import './node_modules/@sweetalert2/theme-dark';
 import '../node_modules/@sweetalert2/theme-dark';
 import styled from 'styled-components';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       GPA Calculator
-//     </div>
-//   );
-// }
-
-// export default App;
+// ===========================================================================
+// STYLED COMPONENTS =========================================================
 
 const Button = styled.button`
     background: #222;
@@ -56,244 +48,247 @@ const Styles = styled.div`
         
     }
 `;
+// ===========================================================================
+// ===========================================================================
+
+
+// ===========================================================================
+// GPA APP ===================================================================
+
+function App() {
+
+  // STATE MANAGEMENT ----------------------------------------------------
+  const [gridInputs, setGridInputs] = useState({
+    grade1: '', credit1: '',
+    grade2: '', credit2: '',
+    grade3: '', credit3: '',
+    grade4: '', credit4: '',
+    grade5: '', credit5: '',
+    grade6: '', credit6: '',
+  });
+
+  const [baseState] = useState(gridInputs);
+  // ----------------------------------------------------------------------
+
+  // CHANGING STATE -------------------------------------------------------
+  const resetToBaseState = () => {
+    setGridInputs(baseState);
+  }
+
+  const handleGradeChange = (grade, e) => {
+    setGridInputs({
+      ...gridInputs, [grade]: e.target.value,
+    });
+  }
+
+  const handleCreditChange = (credit, e) => {
+    setGridInputs({
+      ...gridInputs, [credit]: e.target.value,
+    });
+  }
+  // ---------------------------------------------------------------------
 
 
 
+  // POPUPS ---------------------------------------------------------------
+  const showErrorBox = () => {
+    return (
+      Swal.fire({
+        title: 'Error!',
+        text: "One or more of the inputs is formatted incorrectly, make sure the grade is letter A through F and credits is a number",
+        icon: 'error',
+        cancelButtonText: 'Retry',
+        showCancelButton: true,
+        showConfirmButton: false,
+      })
+    );
+  }
+
+  const showAnswerBox = (answer) => {
+    let message = returnAnswerString(answer);
+    return (
+      Swal.fire({
+        title: message[0],
+        footer: message[1],
+        text: answer.toFixed(2),
+        icon: message[2],
+        cancelButtonText: 'Back',
+        showCancelButton: true,
+        showConfirmButton: false,
+      })
+    );
+  }
+  // ---------------------------------------------------------------------------
 
 
-
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.calculateGPA = this.calculateGPA.bind(this)
-        this.handleGradeChange = this.handleGradeChange.bind(this)
-        this.handleCreditChange = this.handleCreditChange.bind(this)
-        this.resetToBaseState = this.resetToBaseState.bind(this);
-
-
-        this.state = {
-            grade1: '', credit1: '',
-            grade2: '', credit2: '',
-            grade3: '', credit3: '',
-            grade4: '', credit4: '',
-            grade5: '', credit5: '',
-            grade6: '', credit6: '',
-        }
-
-        this.baseState = this.state;
+  // GPA CALCULATION -------------------------------------------------
+  const returnAnswerString = (answer) => {
+    answer = answer.toFixed(2)
+    if (answer >= 3.0) {
+      return [("Impressive! \nYour GPA is"), ("Congrats you're doing great!"), ('success')]
     }
-
-
-
-    showErrorBox() {
-        console.log("Landed on error")
-        return (
-            Swal.fire({
-                title: 'Error!',
-                text: "One or more of the inputs is formatted incorrectly, make sure the grade is letter A through F and credits is a number",
-                icon: 'error',
-                cancelButtonText: 'Retry',
-                showCancelButton: true,
-                showConfirmButton: false,
-            })
-        );
+    else if (answer > 2.5 && answer < 3.0) {
+      return [("Not bad! \nYour GPA is"), ("Keep studying and bump it up!"), ('warning')]
     }
-
-    resetToBaseState() {
-        this.setState(this.baseState);
+    else if (answer <= 2.5) {
+      return [("Keep working hard!\n Your GPA is"), ("Your GPA is quite low but keep trying!"), ('error')]
     }
+  }
 
-    returnAnswerString(answer) {
-        answer = answer.toFixed(2)
-        if (answer >= 3.0) {
-            return [("Impressive! \nYour GPA is"), ("Congrats you're doing great!"), ('success')]
-        }
-        else if (answer > 2.5 && answer < 3.0) {
-            return [("Not bad! \nYour GPA is"), ("Keep studying and bump it up!"), ('warning')]
-        }
-        else if (answer <= 2.5) {
-            return [("Keep working hard!\n Your GPA is"), ("Your GPA is quite low but keep trying!"), ('error')]
-        }
+  const convertGradeToNum = (grade, credit) => {
+    grade = grade.toLowerCase();
+    switch (grade) {
+      case ('a'):
+        return ((credit * 4));
+      case ('b'):
+        return ((credit * 3));
+      case ('c'):
+        return ((credit * 2));
+      case ('d'):
+        return ((credit * 1));
+      case ('f'):
+        return ((credit * 0));
     }
+  }
 
-    showAnswerBox(answer) {
-        let message = this.returnAnswerString(answer);
-        return (
-            Swal.fire({
-                title: message[0],
-                footer: message[1],
-                text: answer.toFixed(2),
-                icon: message[2],
-                cancelButtonText: 'Back',
-                showCancelButton: true,
-                showConfirmButton: false,
-            })
-        );
+  const getCalculation = (gradeArray, credits) => {
+    let sumArray = []
+    for (var i = 0; i < gradeArray.length; i++) {
+      sumArray.push((convertGradeToNum(gradeArray[i], credits[i])))
     }
+    return ((sumArray.reduce((a, b) => a + b, 0)) / credits.reduce((a, b) => a + b, 0));
+  }
 
-
-    checkIfValidGrade(gradeArray) {
-        let newGrades = []
-        for (var i = 0; i < gradeArray.length; i++) {
-            var s = gradeArray[i];
-            if (s.match(/[a-f]/i) && s.length == 1) {
-                newGrades.push(s);
-            }
-            else if (s.length != 0) {
-                this.showErrorBox();
-                return;
-            }
-        }
-        return newGrades;
+  const checkIfValidCredits = (creditArray) => {
+    let newCredits = []
+    for (var i = 0; i < creditArray.length; i++) {
+      var integer = parseInt(creditArray[i], 10);
+      if (Number.isInteger(integer) == true && creditArray[i].length != 0) {
+        newCredits.push(integer);
+      }
+      else if (creditArray[i].length != 0) {
+        showErrorBox()
+        return;
+      }
     }
+    return newCredits;
+  }
 
-    checkIfValidCredits(creditArray) {
-        let newCredits = []
-        for (var i = 0; i < creditArray.length; i++) {
-            var integer = parseInt(creditArray[i], 10);
-            if (Number.isInteger(integer) == true && creditArray[i].length != 0) {
-                newCredits.push(integer);
-            }
-            else if (creditArray[i].length != 0) {
-                this.showErrorBox();
-                return;
-            }
-        }
-        return newCredits;
+  const checkIfValidGrade = (gradeArray) => {
+    let newGrades = []
+    for (var i = 0; i < gradeArray.length; i++) {
+      var s = gradeArray[i];
+      if (s.match(/[a-f]/i) && s.length == 1) {
+        newGrades.push(s);
+      }
+      else if (s.length != 0) {
+        showErrorBox()
+        return;
+      }
     }
+    return newGrades;
+  }
 
-    convertGradeToNum(grade, credit) {
-        grade = grade.toLowerCase();
-        switch (grade) {
-            case ('a'):
-                return ((credit * 4));
-            case ('b'):
-                return ((credit * 3));
-            case ('c'):
-                return ((credit * 2));
-            case ('d'):
-                return ((credit * 1));
-            case ('f'):
-                return ((credit * 0));
-        }
+  const calculateGPA = () => {
+    let grades = [];
+    let credits = [];
+    let finalAnswer = null;
+
+    Object.entries(gridInputs).map(([key, value]) => {
+      if (key.includes("grade")) {
+        grades.push(value)
+      }
+      if (key.includes("credit")) {
+        credits.push(value)
+      }
+    })
+
+    grades = checkIfValidGrade(grades);
+    credits = checkIfValidCredits(credits);
+
+    if (grades !== undefined && credits !== undefined) {
+      finalAnswer = getCalculation(grades, credits);
+      if (!isNaN(finalAnswer)) {
+        showAnswerBox(finalAnswer);
+      }
     }
+  }
+  // ---------------------------------------------------------------------------
 
-    getCalculation(gradeArray, credits) {
-        let sumArray = []
-        for (var i = 0; i < gradeArray.length; i++) {
-            sumArray.push((this.convertGradeToNum(gradeArray[i], credits[i])))
-        }
-        return ((sumArray.reduce((a, b) => a + b, 0)) / credits.reduce((a, b) => a + b, 0));
-    }
+  // RENDERING INDIVIDUAL ROWS
+  const renderFormRow = (grade, credit) => {
+    return (
+      <Row >
+        <Col  >
+          <Styles>
+            <Input name={grade} placeholder={"Letter Grade(A-F)"} value={gridInputs[grade]} onChange={(e) => handleGradeChange(grade, e)} />
+          </Styles>
+        </Col >
+        <Col  >
+          <Styles>
+            <Input name={credit} placeholder={"Credits"} value={gridInputs[credit]} onChange={(e) => handleCreditChange(credit, e)} />
+          </Styles>
+        </Col>
+      </Row>
+    )
+  }
+
+  // RENDERING GRID
+  const renderInputs = () => {
+    return (
+      <div >
+        {renderFormRow("grade1", "credit1")}
+        {renderFormRow("grade2", "credit2")}
+        {renderFormRow("grade3", "credit3")}
+        {renderFormRow("grade4", "credit4")}
+        {renderFormRow("grade5", "credit5")}
+        {renderFormRow("grade6", "credit6")}
+        <br />
+        <Button onClick={calculateGPA} >
+          <div className="w3-opacity">
+            <b>CALCULATE</b>
+          </div>
+        </Button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Button onClick={resetToBaseState} >
+          <div className="w3-opacity">
+            <b>RESET</b>
+          </div>
+        </Button>
+      </div>
+    )
+  }
+
+  //RENDERING INSTRUCTIONS
+  const instructions = () => {
+    return (
+      <div className="display-linebreak">
+        <h4 className="w3">INSTRUCTIONS</h4>
+        <div className="hr" />
+        <p className="w3-text-grey">
+          {"1. For the letter grade column, put in your letter grade(A through F)\n" +
+            "2. For the credits column put how many credits its worth\n" +
+            "3. Leave the rest of the rows blank once you've put all your classes in\n" +
+            "4. Press Calculate when done"}
+        </p>
+      </div>
+    )
+  }
 
 
-    calculateGPA() {
-        let grades = [];
-        let credits = [];
-        let finalAnswer = null;
-        const a = this.state;
-        Object.entries(a).map(([key, value]) => {
-            if (key.includes("grade")) {
-                grades.push(value)
-            }
-            if (key.includes("credit")) {
-                credits.push(value)
-            }
-        })
-        grades = this.checkIfValidGrade(grades);
-        credits = this.checkIfValidCredits(credits);
-        if (grades !== undefined && credits !== undefined) {
-            finalAnswer = this.getCalculation(grades, credits);
-            if (!isNaN(finalAnswer)) {
-                this.showAnswerBox(finalAnswer);
-            }
-        }
-    }
-
-    handleGradeChange(grade, e) {
-        this.setState({
-            [grade]: e.target.value,
-        });
-    }
-
-    handleCreditChange(credit, e) {
-        this.setState({
-            [credit]: e.target.value,
-        });
-    }
-
-    explainCard() {
-        return (
-            <div className="display-linebreak">
-                <h4 className="w3">INSTRUCTIONS</h4>
-                <div className="hr" />
-                <p className="w3-text-grey">
-                    {"1. For the letter grade column, put in your letter grade(A through F)\n" +
-                        "2. For the credits column put how many credits its worth\n" +
-                        "3. Leave the rest of the rows blank once you've put all your classes in\n" +
-                        "4. Press Calculate when done"}
-                </p>
-            </div>
-        );
-    }
-
-    renderFormRow(grade, credit) {
-        return (
-            <Row >
-                <Col  >
-                    <Styles>
-                        <Input name={grade} placeholder={"Letter Grade(A-F)"} value={this.state[grade]} onChange={(e) => this.handleGradeChange(grade, e)} />
-                    </Styles>
-                </Col >
-                <Col  >
-                    <Styles>
-                        <Input name={credit} placeholder={"Credits"} value={this.state[credit]} onChange={(e) => this.handleCreditChange(credit, e)} />
-                    </Styles>
-                </Col>
-            </Row>
-        )
-    }
-
-    renderInputs() {
-        return (
-            <div >
-                {this.renderFormRow("grade1", "credit1")}
-                {this.renderFormRow("grade2", "credit2")}
-                {this.renderFormRow("grade3", "credit3")}
-                {this.renderFormRow("grade4", "credit4")}
-                {this.renderFormRow("grade5", "credit5")}
-                {this.renderFormRow("grade6", "credit6")}
-                <br />
-                <Button onClick={this.calculateGPA} >
-                    <div className="w3-opacity">
-                        <b>CALCULATE</b>
-                    </div>
-                </Button>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <Button onClick={this.resetToBaseState} >
-                    <div className="w3-opacity">
-                        <b>RESET</b>
-                    </div>
-                </Button>
-            </div>
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <Jumbotron message="GPA CALCULATOR" pic="gpaPic" />
-                <Container>
-                    {this.explainCard()}
-                    <br />
-                    {this.renderInputs()}
-                </Container>
-            </div>
-        )
-    }
-
+  return (
+    <div>
+      <Jumbotron />
+      <Container>
+        {instructions()}
+        <br />
+        {renderInputs()}
+      </Container>
+    </div>
+  )
 }
 
 export default App;
+// ===========================================================================
+// ===========================================================================
+
