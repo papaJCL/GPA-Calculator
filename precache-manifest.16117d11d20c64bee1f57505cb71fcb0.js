e6bbc384dc2f149df05f8b4e3c09e5d337f408e7
papaJCL <jeremylessermore@gmail.com>
2020-09-06 22:29:06 -0600

    Updates

---
+++ precache-manifest.16117d11d20c64bee1f57505cb71fcb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a10444dad7212759a1de8f1a8519238",
+    "url": "/GPA-Calculator/index.html"
+  },
+  {
+    "revision": "89f8c2ca9d0ab83cdb39",
+    "url": "/GPA-Calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f52ce22a6eb31aa8fed",
+    "url": "/GPA-Calculator/static/js/2.fd45f515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GPA-Calculator/static/js/2.fd45f515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f8c2ca9d0ab83cdb39",
+    "url": "/GPA-Calculator/static/js/main.829b0479.chunk.js"
+  },
+  {
+    "revision": "5ae8900a698b970d6e15",
+    "url": "/GPA-Calculator/static/js/runtime-main.ad64e7c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GPA-Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GPA-Calculator/precache-manifest.16117d11d20c64bee1f57505cb71fcb0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GPA-Calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
